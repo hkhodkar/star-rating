460de347c58852dfc9c99cb829bddff02461aeed
hatef <hatef.khodkar@hotmail.com>
2022-02-13 16:18:21 +0330

    fix old value should be shown when mouse leave bug

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -1,52 +1,46 @@
-import {
-  Input,
-  OnInit,
-  Component,
-  ViewChild,
-  AfterViewInit,
-} from '@angular/core';
+import { Input, OnInit, Component, ViewChild, AfterViewInit, OnChanges, SimpleChanges, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'star-rating',
-  templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.scss'],
+  selector: "star-rating",
+  templateUrl: "./star-rating.component.html",
+  styleUrls: ["./star-rating.component.scss"]
 })
-export class StarRatingComponent implements OnInit, AfterViewInit {
-  @ViewChild('starElement') starElement: any | undefined;
+export class StarRatingComponent implements OnInit {
+  @ViewChild("starElement") starElement: any | undefined;
 
-  public rate: number = 1;
   public rateArray: Array<number> = [];
-
-  @Input() size: string = '14';
-  @Input() max: number = 1;
-  @Input() value: number = 1;
-  @Input() uncheckedcolor!: string;
+  @Input() size: string = "14";
+  @Input() totalstars: number = 1;
+  @Input() value!: number;
   @Input() checkedcolor!: string;
-  @Input() readonly: boolean = true;
+  @Input() uncheckedcolor!: string;
+  @Input() readonly: string = "true";
+  @Output() rate = new EventEmitter<number>();
+  public selectedRate!: number;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.rateArray = Array(this.max).fill(0);
+    this.rateArray = Array(+this.totalstars).fill(0);
+    this.selectedRate = this.value;
   }
 
-  ngAfterViewInit(): void {}
-
   onMouseEnter(index: number) {
-    if (this.readonly) return;
-    this.value = index + 1;
+    if (this.readonly === "true") return;
+    this.selectedRate = index + 1;
   }
 
   onMouseLeave() {
-    if (this.readonly) return;
-    this.value = this.rate;
+    if (this.readonly === "true") return;
+     this.selectedRate = this.value;
   }
 
-  saveRate(index: number) {
-    if (this.readonly) return;
+  emitRate(index: number) {
+    if (this.readonly === "true") return;
     this.value = index + 1;
-    this.rate = this.value;
+    this.rate.emit(this.value);
   }
+
   trackByFunction(index: number, item: any) {
     if (!item) return null;
     return index;
